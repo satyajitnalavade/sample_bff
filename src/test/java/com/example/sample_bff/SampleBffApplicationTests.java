6c59fe2582321aebc3190e3ac582c4bdaaa64ff4
satyajitnalavade <satyajit.nalavade@gmail.com>
2020-12-19 13:58:57 -0600

    Initial commit

---
+++ src/test/java/com/example/sample_bff/SampleBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sample_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
